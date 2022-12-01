420b3f4960ef6ea06eba889e811560a28a24b806
Yann <lowscaa@gmail.com>
2022-12-01 23:50:28 +0100

    feat: gears effects

--- src/entities/Item.ts
+++ src/entities/Item.ts
@@ -1,13 +1,29 @@
-import { Uuid } from '@/helpers/uuid'
+import { uuid, Uuid } from '@/helpers/uuid'
 
 export interface Item {
     uuid: Uuid<'Inventory'>
     name: string
     description: string
-    effect: typeof GearEffet
+    effects: Effect[]
 }
 
-function GearEffet() {
-    //
+const effects = {
+    'console_log_1': () => console.log('EFFECT 1'),
+    'console_log_2': () => console.log('EFFECT 2'),
+    'console_log_3': () => console.log('EFFECT 3'),
+} as const
+
+export type Effect = keyof typeof effects
+
+export function useGearEffet(effect: Effect) {
+    return effects[effect]()
 }
 
+export function getItem(): Item {
+    return {
+        uuid: uuid(),
+        name: 'Yolo',
+        description: 'description',
+        effects: ['console_log_1', 'console_log_3'],
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { getByName } from './entities/Player'
 import { getColor, Rarity } from './entities/Rarity'
 import getWeightedRandom from '@/helpers/weightedRandom'
 import { createGear } from '@/entities/Gear'
+import { getItem, useGearEffet } from '@/entities/Item'
 
 console.log(getByName('Shu'))
 console.log(getColor('epique'))
@@ -9,3 +10,5 @@ console.log(getWeightedRandom<Rarity>(['legendary', 'mythic'], [.7, .3]))
 console.log(createGear({ type: 'boots' }))
 console.log(import.meta.env.VITE_API_BASE_URL)
 
+const item = getItem()
+item.effects.forEach((effect) => useGearEffet(effect))
