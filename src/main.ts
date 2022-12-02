dd4ebfce4e746179fc6d269950673a11db570cf1
Yann <lowscaa@gmail.com>
2022-12-02 15:22:54 +0100

    chore: console feedback improvement

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import getWeightedRandom from '@/helpers/weightedRandom'
 import { createGear } from '@/entities/Gear'
 import { getItem, useGearEffet } from '@/entities/Item'
 
-console.log(getByName('Shu'))
-console.log(getColor('epique'))
-console.log(getWeightedRandom<Rarity>(['legendary', 'mythic'], [.7, .3]))
-console.log(createGear({ type: 'boots' }))
-console.log(import.meta.env.VITE_API_BASE_URL)
+console.log(`%O player`, getByName('Shu'))
+console.log(`%O createGear`, createGear({ type: 'boots' }))
+
+const rarity = getWeightedRandom<Rarity>(['legendary', 'mythic'], [.7, .3])
+const rarityColor = getColor(rarity)
+console.log(`%c__________`, `background-color: ${ rarityColor }; color:${ rarityColor }`, rarity)
 
 const item = getItem()
 item.effects.forEach((effect) => useGearEffet(effect))
