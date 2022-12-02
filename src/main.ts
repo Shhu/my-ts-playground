20ecade414b7a6ad36169fe447f528297c6fc7e2
Yann <lowscaa@gmail.com>
2022-12-02 15:33:13 +0100

    chore: remove get prefix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { getByName } from './entities/Player'
 import { getColor, Rarity } from './entities/Rarity'
-import getWeightedRandom from '@/helpers/weightedRandom'
+import weightedRandom from '@/helpers/weightedRandom'
 import { createGear } from '@/entities/Gear'
 import { getItem, useGearEffet } from '@/entities/Item'
 
 console.log(`%O player`, getByName('Shu'))
 console.log(`%O createGear`, createGear({ type: 'boots' }))
 
-const rarity = getWeightedRandom<Rarity>(['legendary', 'mythic'], [.7, .3])
+const rarity = weightedRandom<Rarity>(['legendary', 'mythic'], [.7, .3])
 const rarityColor = getColor(rarity)
 console.log(`%c__________`, `background-color: ${ rarityColor }; color:${ rarityColor }`, rarity)
 
