263cf6bec6622999a493a11f1a22bf4ef5e5acaa
Yann <lowscaa@gmail.com>
2022-12-03 19:52:26 +0100

    chore: structure test inventory

--- src/entities/Inventory.test.ts
+++ src/entities/Inventory.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest'
 
-describe('player can retrieve his inventory', () => {
-    it.skip('', () => {
+describe('player inventory', () => {
+    it.skip('can add item to player inventory', () => {
         expect(true).toBe(false)
     })
 })
