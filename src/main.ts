c2e72214a70484038f3c9a55ee0153e106559297
Yann <lowscaa@gmail.com>
2022-12-03 18:17:17 +0100

    chore: use tests to test

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { getByName } from './entities/Player'
-import { getColor, Rarity } from './entities/Rarity'
-import weightedRandom from '@/helpers/weightedRandom'
-
-console.log(`%O player`, getByName('Shu'))
-
-const rarity = weightedRandom<Rarity>(['legendary', 'mythic'], [.7, .3])
-const rarityColor = getColor(rarity)
-console.log(`%c__________`, `background-color: ${ rarityColor }; color:${ rarityColor }`, rarity)
