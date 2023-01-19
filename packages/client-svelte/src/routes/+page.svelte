542c7934d148615730325f26a4355148312e60cd
Shhu <aubriotyann@gmail.com>
2023-01-19 23:50:12 +0100

    feat: testing svelte transitions

--- packages/client-svelte/src/routes/+page.svelte
+++ packages/client-svelte/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 import { trpc } from '$lib/trpc/client'
+import { fly } from 'svelte/transition'
 
 async function fetch() {
     return await trpc().getUserById.query({
@@ -14,13 +15,15 @@ let player = fetch()
     <h1 class='text-3xl font-bold underline text-blue-500'>
         Hello
     </h1>
-    <div class='w-full max-w-xl mx-auto bg-gray-100 rounded-lg my-8 p-4'>
-        {#await player}
-            <p>...waiting</p>
-        {:then { player }}
-            <pre class='text-sm'>{JSON.stringify(player)}</pre>
-        {:catch error}
-            <p style='color: red'>{error.message}</p>
-        {/await}
-    </div>
+    {#await player}
+        <p>...waiting</p>
+    {:then { player }}
+        {#if player}
+            <div in:fly='{({ y: 200, duration: 200 })}' class='w-full max-w-xl mx-auto bg-gray-100 rounded-lg my-8 p-4'>
+                <pre class='text-sm'>{JSON.stringify(player)}</pre>
+            </div>
+        {/if}
+    {:catch error}
+        <p style='color: red'>{error.message}</p>
+    {/await}
 </div>
