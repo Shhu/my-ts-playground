32d71f7a44905f39a073700696b7bd33b3a70148
Shhu <aubriotyann@gmail.com>
2023-01-20 18:33:13 +0100

    chore: improve syntax

--- packages/server/router.ts
+++ packages/server/router.ts
@@ -1,12 +1,12 @@
 import { initTRPC } from '@trpc/server'
-import { Prisma } from '@prisma/client'
+import type { Prisma } from '@prisma/client'
 import { z } from 'zod'
 import { prisma } from './prisma'
 
-const defaultUserSelect = Prisma.validator<Prisma.PlayerSelect>()({
+const playerSelect: Prisma.PlayerSelect = {
     id: true,
     name: true,
-})
+}
 
 const t = initTRPC.create()
 export const appRouter = t.router({
@@ -17,7 +17,7 @@ export const appRouter = t.router({
         .query(async ({ input }) => {
             const player = await prisma.player.findUnique({
                 where: { id: input.id },
-                select: defaultUserSelect,
+                select: playerSelect,
             })
 
             return { player }
